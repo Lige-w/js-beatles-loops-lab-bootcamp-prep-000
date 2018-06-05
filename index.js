8ee4e777acb47d0ad32619071d11bbcd601e2ac5
Lige <lige.williamson@gmail.com>
2018-06-05 13:20:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var n = 0; n < 4; n++) {
-    array.push(musicians[n] + " plays " + instruments[n]);
-  }
-  return array;
-}
-
-function johnLennonFacts(facts) {
-  var array = [];
-  while (n < 10) {
-    array.push(facts[n] + "!!!");
-    n++;
-  }
-  return array
-}
+// function theBeatlesPlay(musicians, instruments) {
+//   var array = [];
+//   for (var n = 0; n < 4; n++) {
+//     array.push(musicians[n] + " plays " + instruments[n]);
+//   }
+//   return array;
+// }
+// 
+// function johnLennonFacts(facts) {
+//   var array = [];
+//   while (n < 10) {
+//     array.push(facts[n] + "!!!");
+//     n++;
+//   }
+//   return array
+// }
